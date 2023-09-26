761e4b7055a16086d90665164c13ed6faf1a899f
veera-2 <naveera.nh@gmail.com>
2023-09-27 01:20:41 +0200

    escape sequences

---
+++ c-files/escape_sq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // using escape sequences
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"i love you\" - that is what he said to me\n");
+    printf("\\mine\\  love\n");
+    printf("\/his or hers\/");
+    return 0;
+}
